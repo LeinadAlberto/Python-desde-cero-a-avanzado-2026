c7f2adc66e6741ad2aa3534e6399ebaed951738e
Alberto <dany.canaviri2022@gmail.com>
2026-07-30 12:19:13 -0400

    Constantes en Python, completado

---
+++ 06_constantes.py
@@ -0,0 +1,18 @@
+"""
+    Constantes: 
+
+    - En Python no existe el concepto de constantes
+    - Se crea constantes utilizando la nomenclatura snake_case
+    - El nombre de la variable debe tener todos los caracteres en mayúscula
+    - Al ser una variable se puede modificar su valor posteriormente algo
+      que en la teoria no deberia permitir por ser una constante, pero esto
+      solo se hace con fines de lectura.
+"""
+
+VERSION = 3.14
+
+VERSION = 3.17
+
+print()
+print(VERSION)
+print(type(VERSION))
