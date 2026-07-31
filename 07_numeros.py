be9dbbd635b49ea348928bc2458d648489300f16
Alberto <dany.canaviri2022@gmail.com>
2026-07-30 23:39:51 -0400

    Números en Python, completado

---
+++ 07_numeros.py
@@ -0,0 +1,22 @@
+# int
+number_1 = 10 
+
+#result_1 = number + 5
+
+#result_1 = number - 5
+
+#result_1 = number * 5
+
+result_1 = number_1 / 2 # Cuando se utiliza / para la división, se obtendra un valor flotante.
+
+print()
+print("El resultado es:", result_1) # El resultado es: 5.0
+print(type(result_1)) # <class 'float'
+
+number_2 = 20
+
+result_2 = number_2 // 10 # Cuando se utiliza // para la división, se obtendra un valor entero.
+
+print()
+print("El resultado es:", result_2) # El resultado es: 2
+print(type(number_2)) # <class 'int'>
