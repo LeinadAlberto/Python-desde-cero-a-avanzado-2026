8e5f5c392e87032552ccac70421d2e2aa14ff498
Alberto <dany.canaviri2022@gmail.com>
2026-08-04 23:09:36 -0400

    Pedir valores por teclado, completado

---
+++ 10_pedir_valores_por_teclado.py
@@ -0,0 +1,12 @@
+"""
+    - La función input() en Python permite leer datos que el usuario escribe 
+      desde el teclado. Cuando se ejecuta, el programa se detiene y espera 
+      hasta que el usuario escriba algo y presione Enter.
+    - input() devuelve una cadena de texto (str), incluso si el usuario escribe 
+      un número.
+"""
+
+
+full_name = input("Ingresa tu nombre: ")
+
+print("Hola", full_name)
