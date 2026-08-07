32e7e1716bcd1a7efa9b5ad5b4a72b429a007433
Alberto <dany.canaviri2022@gmail.com>
2026-08-06 23:42:51 -0400

    Multiples Variables, completado

---
+++ 12_multiples_variables.py
@@ -0,0 +1,15 @@
+# first_name = "Daniel Alberto"
+# edad = 43
+# is_active = True
+
+# print(first_name)
+# print(edad)
+# print(is_active)
+
+# Existe otra forma de crear estas 3 variables y seria creando una sola línea de código.
+
+first_name, edad, is_active = "Daniel Alberto", 43, True
+
+print(first_name)
+print(edad)
+print(is_active)
